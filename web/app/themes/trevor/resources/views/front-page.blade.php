68c4af1c91fb4c55dfa22594c7ee9a1fcfcff627
Aaron Cimolini <aaron@cimolini.com>
2018-09-29 00:24:16 -0700

    Project theme start.

---
+++ web/app/themes/trevor/resources/views/front-page.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+  <div id="hero">
+    hero
+    main benefit statement
+    call to action
+  </div>
+  <div id="process">
+    process?
+  </div>
+  <div id="portraiture">
+    portraiture
+    description of process
+    gallery of images
+  </div>
+  <div id="editorial">
+    editorial
+    description of process
+    gallery of images
+  </div>
+  <div id="next-steps">
+    next steps
+    overcome objections
+    Just call me, let's do coffee, everyone's nervous about getting their photo taken.
+  </div>
+  <div id="contact">
+    Contact info
+    form
+    contact details
+  </div>
+@endsection
+
+
+
---
+++ web/app/themes/trevor/resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header class="banner">
+  <div class="container">
+    <a class="brand" href="{{ home_url('/') }}">
+      {{ get_bloginfo('name', 'display') }}
+      <br />
+      {{ get_option('blogdescription') }}
+    </a>
+    <nav class="nav-primary">
+      <a class="js-contact" href="#contact">Contact</a>
+    </nav>
+  </div>
+</header>
